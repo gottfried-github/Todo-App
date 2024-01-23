93e04e6e43b1efcee379701526d16a584a5c8cbd
gottfried <gottfried.dev@gmail.com>
2024-01-23 14:12:51 +0200

    sketch out rendering logic

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ function itemUpdateDone(done, i, items) {
     )
 }
 
-function itemsActive(items) {
-    return items.reduce((itemsActive, item, _i) => {
-        if (item.done) return itemsActive
+function itemsDeleteDone(items) {
+    return items.reduce((itemsNotDone, item) => {
+        if (item.done) return itemsNotDone
 
-        itemsActive.push({
+        itemsNotDone.push({...item})
+    }, [])
+}
+
+function itemsNotDone(items) {
+    return items.reduce((itemsNotDone, item, _i) => {
+        if (item.done) return itemsNotDone
+
+        itemsNotDone.push({
             i: _i, item: {...item}
         })
     }, [])
@@ -26,6 +34,89 @@ function itemsDone(items) {
     }, [])
 }
 
-function main() {
+function itemsFormat(items) {
+    return items.map((item, i) => ({i, item: {...item}}))
+}
 
+function itemRender(item, {doneCb, notDoneCb}) {
+    // doneCb(item.i)
+    // notDoneCb(item.i)
+}
+
+function itemsRender(items, {
+    doneCb, notDoneCb, 
+    showDoneCb, showNotDoneCb, 
+    deleteDoneCb
+}) {
+    
+}
+
+function main() {
+    let items = [
+        {
+            done: false,
+            title: "item 0"
+        },
+        {
+            done: true,
+            title: "item 1"
+        },
+        {
+            done: true,
+            title: "item 2"
+        },
+        {
+            done: false,
+            title: "item 3"
+        }
+    ]
+
+    let showAll = true
+    let showDone = null
+
+    itemsRender(itemsFormat(items), {
+        doneCb: (i) => {
+            items = itemUpdateDone(true, i, items)
+
+            if (null === showDone) throw new Error()
+
+            itemsRender(
+                showAll 
+                    ? itemsFormat(items)
+                    : showDone
+                        ? itemsDone(items)
+                        : itemsNotDone(items)
+            )
+        },
+        notDoneCb: () => {
+            items = itemUpdateDone(true, i, items)
+
+            itemsRender(itemsFormat)
+        },
+        showDoneCb: () => {
+            showAll = false
+            showDone = true
+
+            itemsRender(itemsDone(items))
+        }, 
+        showNotDoneCb: () => {
+            showAll = false
+            showDone = false
+
+            itemsRender(itemsNotDone(items))
+        },
+        deleteDoneCb: () => {
+            items = itemsDeleteDone(items)
+
+            if (null === showDone) throw new Error()
+
+            itemsRender(
+                showAll 
+                    ? itemsFormat(items)
+                    : showDone
+                        ? itemsDone(items)
+                        : itemsNotDone(items)
+            )
+        }, 
+    })
 }
