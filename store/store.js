67953549239999ccf7fa83e5954b77e86864a0f2
gottfried <gottfried.dev@gmail.com>
2024-01-26 11:55:08 +0200

    Storage: use arrow functions instead of methods and dont use bind

--- store/store.js
+++ store/store.js
@@ -6,8 +6,6 @@ const FILTERS = ["all", "done", "notDone"]
 
 export class Store {
     constructor() {
-        this._subscriptions = {}
-
         this.state = {
             items: [],
             filter: "all"
@@ -16,33 +14,27 @@ export class Store {
         this.init()
     }
 
-    init() {
-        this._subscriptions._append = this._append.bind(this)
-        this._subscriptions._updateStatus = this._updateStatus.bind(this)
-        this._subscriptions._delete = this._delete.bind(this)
-        this._subscriptions._deleteDone = this._deleteDone.bind(this)
-        this._subscriptions._setFilter = this._setFilter.bind(this)
-
-        EventEmitter.subscribe(Events.ITEM_APPEND_ONE, this._subscriptions._append)
-        EventEmitter.subscribe(Events.ITEM_UPDATE_STATUS_ONE, this._subscriptions._updateStatus)
-        EventEmitter.subscribe(Events.ITEM_DELETE_ONE, this._subscriptions._delete)
-        EventEmitter.subscribe(Events.ITEM_DELETE_DONE, this._subscriptions._deleteDone)
-        EventEmitter.subscribe(Events.SET_FILTER, this._subscriptions._setFilter)
+    init = () => {
+        EventEmitter.subscribe(Events.ITEM_APPEND_ONE, this._append)
+        EventEmitter.subscribe(Events.ITEM_UPDATE_STATUS_ONE, this._updateStatus)
+        EventEmitter.subscribe(Events.ITEM_DELETE_ONE, this._delete)
+        EventEmitter.subscribe(Events.ITEM_DELETE_DONE, this._deleteDone)
+        EventEmitter.subscribe(Events.SET_FILTER, this._setFilter)
     }
 
-    unsubscribe() {
-        EventEmitter.unsubscribe(Events.ITEM_APPEND_ONE, this._subscriptions._append)
-        EventEmitter.unsubscribe(Events.ITEM_UPDATE_STATUS_ONE, this._subscriptions._updateStatus)
-        EventEmitter.unsubscribe(Events.ITEM_DELETE_ONE, this._subscriptions._delete)
-        EventEmitter.unsubscribe(Events.ITEM_DELETE_DONE, this._subscriptions._deleteDone)
-        EventEmitter.unsubscribe(Events.SET_FILTER, this._subscriptions._setFilter)
+    unsubscribe = () => {
+        EventEmitter.unsubscribe(Events.ITEM_APPEND_ONE, this._append)
+        EventEmitter.unsubscribe(Events.ITEM_UPDATE_STATUS_ONE, this._updateStatus)
+        EventEmitter.unsubscribe(Events.ITEM_DELETE_ONE, this._delete)
+        EventEmitter.unsubscribe(Events.ITEM_DELETE_DONE, this._deleteDone)
+        EventEmitter.unsubscribe(Events.SET_FILTER, this._setFilter)
     }
 
-    _append(label) {
+    _append = (label) => {
         this.state.items.push(new Item(label, false))
     }
 
-    _updateStatus({id, done}) {
+    _updateStatus = ({id, done}) => {
         this.state.items = this.state.items.map(item => {
             if (id === item.id) {
                 return {
@@ -54,15 +46,15 @@ export class Store {
         })
     }
     
-    _delete(id) {
+    _delete = (id) => {
         this.state.items = this.state.items.filter(item => id !== item.id)
     }
     
-    _deleteDone() {
+    _deleteDone = () => {
         this.state.items = this.state.items.filter(item => !item.done)
     }
 
-    _setFilter(filter) {
+    _setFilter = (filter) => {
         if (!FILTERS.includes(filter)) {
             throw new Error("invalid filter name")
         }
@@ -70,7 +62,7 @@ export class Store {
         this.state.filter = filter
     }
 
-    getCount(filter) {
+    getCount = (filter) => {
         if (!filter) return this.getItems().length
 
         switch (filter) {
@@ -88,7 +80,7 @@ export class Store {
         }
     }
 
-    getItems(filter) {
+    getItems = (filter) => {
         const _filter = filter || this.state.filter
 
         switch (_filter) {
@@ -106,7 +98,7 @@ export class Store {
         }
     }
 
-    getFilter() {
+    getFilter = () => {
         return this.state.filter
     }
 }
