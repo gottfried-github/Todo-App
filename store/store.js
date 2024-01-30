4dc7939531dc678a172014cb7a76c71f40e5f0f7
gottfried <gottfried.dev@gmail.com>
2024-01-30 14:21:40 +0200

    Store, deepEqual: fix bugs; adapt _setState

--- store/store.js
+++ store/store.js
@@ -11,7 +11,7 @@ class Item {
   }
 }
 
-function deepEqual(newV, oldV) {
+export function deepEqual(newV, oldV) {
   if (Array.isArray(newV) && Array.isArray(oldV)) {
     if (newV.length !== oldV.length) {
       return false
@@ -20,7 +20,7 @@ function deepEqual(newV, oldV) {
         return deepEqual(item, oldV[i])
       })
 
-      return equal.includes(false)
+      return !equal.includes(false)
     }
   } else if (
     null !== newV &&
@@ -39,7 +39,7 @@ function deepEqual(newV, oldV) {
 
     const equal = keysNew.map((k) => deepEqual(newV[k], oldV[k]))
 
-    return equal.includes(false)
+    return !equal.includes(false)
   } else {
     return newV === oldV
   }
@@ -73,32 +73,22 @@ export class Store {
   }
 
   _setState(state) {
-    if (
-      (!state.filter || this.state.filter === state.filter) &&
-      (!state.items || this.state.items === state.items)
-    ) {
-      return
-    } else {
-      if (state.filter && this.state.filter !== state.filter) {
-        this.state.filter = state.filter
-      }
+    if (deepEqual(state, this.state)) return
 
-      if (state.items && this.state.items !== state.items) {
-        this.state.items = state.items
-      }
-
-      EventEmitter.emit({ type: Events.STORAGE_UPDATED })
-    }
+    this.state = state
+    EventEmitter.emit({ type: Events.STORAGE_UPDATED })
   }
 
   _append = (label) => {
     this._setState({
+      ...this.state,
       items: [...this.state.items, new Item(label, false)],
     })
   }
 
   _updateStatus = ({ id }) => {
     this._setState({
+      ...this.state,
       items: this.state.items.map((item) => {
         if (id === item.id) {
           return {
@@ -114,6 +104,7 @@ export class Store {
 
   _updateLabel = ({ id, label }) => {
     this._setState({
+      ...this.state,
       items: this.state.items.map((item) => {
         if (id === item.id) {
           return {
@@ -129,12 +120,14 @@ export class Store {
 
   _delete = (id) => {
     this._setState({
+      ...this.state,
       items: this.state.items.filter((item) => id !== item.id),
     })
   }
 
   _deleteDone = () => {
     this._setState({
+      ...this.state,
       items: this.state.items.filter((item) => !item.done),
     })
   }
@@ -144,7 +137,7 @@ export class Store {
       throw new Error('invalid filter name')
     }
 
-    this._setState({ filter })
+    this._setState({ ...this.state, filter })
   }
 
   getCount = (filter) => {
