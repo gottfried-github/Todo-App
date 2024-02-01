6465b7c8ce989eac9171e936da0d36cd3f77dceb
gottfried <gottfried.dev@gmail.com>
2024-02-01 21:35:52 +0200

    controllers, updateName: handle errors, assign status

--- server/controllers/updateName.js
+++ server/controllers/updateName.js
@@ -1,7 +1,34 @@
+import mongoose from 'mongoose'
+import { ResponseData } from '../utils/utils.js'
+
 import Todo from '../models/todo.js'
 
 export default async function updateName({ id, name }) {
-  const todo = await Todo.findById(id)
+  let item = null
 
-  return todo.updateName(name)
+  try {
+    item = await Todo.findById(id)
+  } catch (e) {
+    if (e instanceof mongoose.Error.CastError) {
+      return new ResponseData(400, e)
+    } else {
+      return new ResponseData(500, e)
+    }
+  }
+
+  if (!item) {
+    return new ResponseData(404)
+  }
+
+  try {
+    await item.updateName(name)
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError) {
+      return new ResponseData(400, e)
+    }
+
+    return new ResponseData(500, e)
+  }
+
+  return new ResponseData(200, item)
 }
