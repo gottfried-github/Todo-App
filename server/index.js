04d350d139d20f90b093d386b34cdcda9d3b0037
gottfried <gottfried.dev@gmail.com>
2024-02-02 14:07:55 +0200

    wip, server: start writing the server - handle non-existent endpoint

--- server/index.js
+++ server/index.js
@@ -1,5 +1,19 @@
+import http from 'http'
 import mongoose from 'mongoose'
+
+import { CONTROLLERS } from './controllers-mapper.js'
 
 async function main() {
   await mongoose.connect(process.env.DB_CONNECTION)
+
+  http.createServer((req, res) => {
+    if (!(req.url in CONTROLLERS)) {
+      res.statusCode = 404
+      res.setHeader('Content-Type', 'application/json')
+      return res.end(JSON.stringify({ data: { message: "endpoint doesn't exist" } }))
+    }
+
+    if ('GET' == req.method) {
+    }
+  })
 }
