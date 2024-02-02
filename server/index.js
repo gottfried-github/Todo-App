07d1711782a62c7a37762b27914b322664203036
gottfried <gottfried.dev@gmail.com>
2024-02-02 14:36:47 +0200

    wip, server: handle POST

--- server/index.js
+++ server/index.js
@@ -13,12 +13,10 @@ async function main() {
       if (!(req.url in CONTROLLERS.GET)) {
         res.statusCode = 404
 
-        return res.end(
-          JSON.stringify({ data: { message: "endpoint doesn't exist or wrong HTTP method" } })
-        )
+        return res.end(JSON.stringify({ message: "endpoint doesn't exist or wrong HTTP method" }))
       }
 
-      const _res = CONTROLLERS.GET[req.url]
+      const _res = CONTROLLERS.GET[req.url]()
 
       res.statusCode = _res.status
 
@@ -27,10 +25,40 @@ async function main() {
       if (!(req.url in CONTROLLERS.POST)) {
         res.statusCode = 404
 
+        return res.end(JSON.stringify({ message: "endpoint doesn't exist or wrong HTTP method" }))
+      }
+
+      if ('application/json' !== req.headers['content-type']) {
+        res.statusCode = 400
+
         return res.end(
-          JSON.stringify({ data: { message: "endpoint doesn't exist or wrong HTTP method" } })
+          JSON.stringify({ message: "server only supports 'application/json' content-type" })
         )
       }
+
+      const dataRawChunks = []
+
+      req
+        .on('data', chunk => {
+          dataRawChunks.push(chunk)
+        })
+        .on('end', () => {
+          const body = Buffer.concat(body).toString()
+
+          const _res = CONTROLLERS.POST[req.url](body)
+
+          res.statusCode = _res.status
+
+          res.end(JSON.stringify(_res.data))
+        })
+    } else {
+      res.statusCode = 404
+
+      return res.end(
+        JSON.stringify({
+          data: { message: `server doesn't support the ${req.method} HTTP method` },
+        })
+      )
     }
   })
 }
