5cab39122863c5f50e9e27a9ab54dd92de832a61
gottfried <gottfried.dev@gmail.com>
2024-02-02 14:52:55 +0200

    server: listen; reformat

--- server/index.js
+++ server/index.js
@@ -9,70 +9,72 @@ const CONTENT_TYPE = 'application/json'
 async function main() {
   await mongoose.connect(process.env.DB_CONNECTION)
 
-  http.createServer((req, res) => {
-    res.on('error', error => {
+  http
+    .createServer((req, res) => {
+      res.on('error', error => {
+        res.setHeader('Content-Type', CONTENT_TYPE)
+
+        res.end(JSON.stringify(error))
+      })
+
       res.setHeader('Content-Type', CONTENT_TYPE)
 
-      res.end(JSON.stringify(error))
-    })
+      if ('GET' == req.method) {
+        if (!(req.url in CONTROLLERS.GET)) {
+          res.statusCode = 404
 
-    res.setHeader('Content-Type', CONTENT_TYPE)
+          return res.end(JSON.stringify({ message: "endpoint doesn't exist or wrong HTTP method" }))
+        }
 
-    if ('GET' == req.method) {
-      if (!(req.url in CONTROLLERS.GET)) {
+        const _res = CONTROLLERS.GET[req.url]()
+
+        res.statusCode = _res.status
+
+        return res.end(JSON.stringify(_res.data))
+      } else if ('POST' === req.method) {
+        if (!(req.url in CONTROLLERS.POST)) {
+          res.statusCode = 404
+
+          return res.end(JSON.stringify({ message: "endpoint doesn't exist or wrong HTTP method" }))
+        }
+
+        if (CONTENT_TYPE !== req.headers['content-type']) {
+          res.statusCode = 400
+
+          return res.end(
+            JSON.stringify({ message: `server only supports ${CONTENT_TYPE} content-type` })
+          )
+        }
+
+        const dataRawChunks = []
+
+        return req
+          .on('error', error => {
+            res.statusCode = 500
+
+            res.end(JSON.stringify(error))
+          })
+          .on('data', chunk => {
+            dataRawChunks.push(chunk)
+          })
+          .on('end', () => {
+            const body = Buffer.concat(body).toString()
+
+            const _res = CONTROLLERS.POST[req.url](body)
+
+            res.statusCode = _res.status
+
+            res.end(JSON.stringify(_res.data))
+          })
+      } else {
         res.statusCode = 404
-
-        return res.end(JSON.stringify({ message: "endpoint doesn't exist or wrong HTTP method" }))
-      }
-
-      const _res = CONTROLLERS.GET[req.url]()
-
-      res.statusCode = _res.status
-
-      return res.end(JSON.stringify(_res.data))
-    } else if ('POST' === req.method) {
-      if (!(req.url in CONTROLLERS.POST)) {
-        res.statusCode = 404
-
-        return res.end(JSON.stringify({ message: "endpoint doesn't exist or wrong HTTP method" }))
-      }
-
-      if (CONTENT_TYPE !== req.headers['content-type']) {
-        res.statusCode = 400
 
         return res.end(
-          JSON.stringify({ message: `server only supports ${CONTENT_TYPE} content-type` })
+          JSON.stringify({
+            data: { message: `server doesn't support the ${req.method} HTTP method` },
+          })
         )
       }
-
-      const dataRawChunks = []
-
-      return req
-        .on('error', error => {
-          res.statusCode = 500
-
-          res.end(JSON.stringify(error))
-        })
-        .on('data', chunk => {
-          dataRawChunks.push(chunk)
-        })
-        .on('end', () => {
-          const body = Buffer.concat(body).toString()
-
-          const _res = CONTROLLERS.POST[req.url](body)
-
-          res.statusCode = _res.status
-
-          res.end(JSON.stringify(_res.data))
-        })
-    } else {
-      res.statusCode = 404
-
-      return res.end(
-        JSON.stringify({
-          data: { message: `server doesn't support the ${req.method} HTTP method` },
-        })
-      )
-    }
-  })
+    })
+    .listen(process.env.HTTP_PORT)
 }
