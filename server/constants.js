c9c69a8a58524ece93b820883118d88c7b62f416
gottfried <gottfried.dev@gmail.com>
2024-02-05 22:21:46 +0200

    server, PUT: move server handling logic to controller

---
+++ server/constants.js
@@ -0,0 +1,1 @@
+export const CONTENT_TYPE = 'application/json'
--- server/controllers/create.js
+++ server/controllers/create.js
@@ -1,20 +1,46 @@
 import mongoose from 'mongoose'
-import { ResponseData } from '../utils/utils.js'
+
+import { CONTENT_TYPE } from '../constants.js'
+import { parseBody } from '../utils/utils.js'
 
 import Todo from '../models/todo.js'
 
-export default async function create(data) {
-  const todo = new Todo(data)
+export default async function create(req, res) {
+  return parseBody(req, async body => {
+    if (!body) {
+      res.statusCode = 400
 
-  try {
-    await todo.save()
-  } catch (e) {
-    if (e instanceof mongoose.Error.ValidationError) {
-      return new ResponseData(400, e)
+      return res.end(JSON.stringify({ message: 'no item data specified' }))
     }
 
-    return new ResponseData(500, e)
-  }
+    if (CONTENT_TYPE !== req.headers['content-type']) {
+      res.statusCode = 400
 
-  return new ResponseData(201, todo)
+      return res.end(
+        JSON.stringify({ message: `server only supports ${CONTENT_TYPE} content-type` })
+      )
+    }
+
+    let item = new Todo(JSON.parse(body))
+
+    try {
+      item = await item.save()
+    } catch (e) {
+      if (e instanceof mongoose.Error.ValidationError) {
+        res.statusCode = 400
+
+        return res.end(JSON.stringify(e))
+      }
+
+      console.log(`Server, create, database errored - error:`, e)
+
+      res.statusCode = 500
+
+      return res.end(JSON.stringify(e))
+    }
+
+    res.statusCode = 201
+
+    res.end(JSON.stringify(item))
+  })
 }
