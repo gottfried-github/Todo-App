b46b43b88e748cae478f87b822255ab0d2c400c8
gottfried <gottfried.dev@gmail.com>
2024-02-05 23:08:21 +0200

    server: move routing into a separate file

--- server/index.js
+++ server/index.js
@@ -2,13 +2,7 @@ import http from 'http'
 import mongoose from 'mongoose'
 
 import { CONTENT_TYPE } from './constants.js'
-import { parseUrl } from './utils/utils.js'
-
-import create from './controllers/create.js'
-import update from './controllers/update.js'
-import deleteById from './controllers/deleteById.js'
-import deleteDone from './controllers/deleteDone.js'
-import getAll from './controllers/getAll.js'
+import router from './router.js'
 
 async function main() {
   await mongoose.connect(process.env.DB_CONNECTION)
@@ -30,57 +24,7 @@ async function main() {
 
       res.setHeader('Content-Type', CONTENT_TYPE)
 
-      const params = parseUrl(req.url)
-
-      if (!params) {
-        res.statusCode = 404
-
-        return res.end(JSON.stringify({ message: "endpoint doesn't exist" }))
-      }
-
-      req.params = params
-
-      if ('GET' == req.method) {
-        return getAll(req, res)
-      } else if ('PUT' === req.method) {
-        if (req.params.id) {
-          res.statusCode = 400
-
-          return res.end(
-            JSON.stringify({
-              message: 'PUT with an existing item is not supported. Use PATCH instead',
-            })
-          )
-        }
-
-        return create(req, res)
-      } else if ('PATCH' === req.method) {
-        if (!req.params.id) {
-          res.statusCode = 400
-
-          return res.end(JSON.stringify({ message: 'no item specified' }))
-        }
-
-        return update(req, res)
-      } else if ('DELETE' === req.method) {
-        if (!req.params.id) {
-          return deleteDone(req, res)
-        } else {
-          return deleteById(req, res)
-        }
-      } else if ('OPTIONS' === req.method) {
-        res.statusCode = 200
-
-        res.end()
-      } else {
-        res.statusCode = 404
-
-        return res.end(
-          JSON.stringify({
-            data: { message: `server doesn't support the ${req.method} HTTP method` },
-          })
-        )
-      }
+      router(req, res)
     })
     .listen(process.env.HTTP_PORT)
 }
