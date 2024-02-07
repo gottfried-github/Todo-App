43721168f5d42fd9bde0aa2628a91c7c97441023
gottfried <gottfried.dev@gmail.com>
2024-02-07 18:44:35 +0200

    wip: write the app with Koa; set up content-type restriction and bodyParser

--- server/index.js
+++ server/index.js
@@ -1,12 +1,48 @@
-import http from 'http'
 import mongoose from 'mongoose'
 
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+
 import { CONTENT_TYPE } from './constants.js'
-import router from './router.js'
+// import router from './router.js'
 
 async function main() {
   await mongoose.connect(process.env.DB_CONNECTION)
 
+  const app = new Koa()
+
+  app
+    .use(async (ctx, next) => {
+      if (
+        !('content-type' in ctx.request.headers) ||
+        ctx.request.headers['content-type'] !== CONTENT_TYPE
+      ) {
+        ctx.status = 415
+
+        ctx.body = {
+          message: `only ${CONTENT_TYPE} content-type is supported`,
+        }
+
+        return
+      }
+
+      await next()
+    })
+    .use(
+      bodyParser({
+        enableTypes: 'json',
+      })
+    )
+    .use(ctx => {
+      ctx.body = {
+        message: 'some response',
+      }
+    })
+    .listen(process.env.HTTP_PORT, () => {
+      console.log(`server is running at port ${process.env.HTTP_PORT}`)
+    })
+
+  /*
   http
     .createServer(async (req, res) => {
       res.on('error', error => {
@@ -29,6 +65,7 @@ async function main() {
     .listen(process.env.HTTP_PORT, () => {
       console.log(`server listening on port ${process.env.HTTP_PORT}`)
     })
+  */
 }
 
 main()
