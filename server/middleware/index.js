2a15d0df64e0445484399fe311e22d9ae28d0353
gottfried <gottfried.dev@gmail.com>
2024-02-08 15:19:07 +0200

    server: remove CONTENT_TYPE

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,7 +1,5 @@
 import bodyParser from 'koa-bodyparser'
 
-import { CONTENT_TYPE } from '../constants.js'
-
 export const parseBody = bodyParser({
   enableTypes: 'json',
 })
@@ -9,12 +7,12 @@ export const parseBody = bodyParser({
 export const validateContentType = async (ctx, next) => {
   if (
     !('content-type' in ctx.request.headers) ||
-    ctx.request.headers['content-type'] !== CONTENT_TYPE
+    ctx.request.headers['content-type'] !== 'application/json'
   ) {
     ctx.status = 415
 
     ctx.body = {
-      message: `only ${CONTENT_TYPE} content-type is supported`,
+      message: 'only application/json content-type is supported',
     }
 
     return
