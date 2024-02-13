92a13abe156368172ca2d63cb4d329602acfaec7
gottfried <gottfried.dev@gmail.com>
2024-02-13 14:53:04 +0200

    client, components: add Controls

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react'
 
 import Input from './components/Input'
+import Controls from './components/Controls'
 import Items from './components/Items'
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="container">
         <h1 className="heading">todo list</h1>
         <Input />
+        <Controls />
         <Items />
       </div>
     )
