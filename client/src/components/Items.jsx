425bd74926a47810c2817ac85f753d4d645371d2
gottfried <gottfried.dev@gmail.com>
2024-02-19 22:53:16 +0200

    wip, client: start implementing Items with DataGrid

--- client/src/components/Items.jsx
+++ client/src/components/Items.jsx
@@ -2,28 +2,162 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import styled from '@emotion/styled'
 
+import TextField from '@mui/material/TextField'
+import { DataGrid, useGridApiRef } from '@mui/x-data-grid'
+import { ITEM_STATUS } from '../constants'
+
 import slice from '../store/slice'
 
 import Item from './Item'
 
 export default function Items() {
+  const gridApiRef = useGridApiRef()
+
   const [editingId, setEditingId] = useState(null)
 
   const items = useSelector(state => slice.selectors.selectItems({ [slice.reducerPath]: state }))
 
   const handleEdit = itemId => {
-    if (!editingId || editingId !== itemId) {
-      setEditingId(itemId)
-    } else {
-      setEditingId(null)
+    const _editingId = !editingId || editingId !== itemId ? itemId : null
+
+    if (!editingId) {
+      gridApiRef.current.updateRows([
+        {
+          id: itemId,
+          name: {
+            ...gridApiRef.current.getCellValue(itemId, 'name'),
+            isEditing: true,
+          },
+        },
+      ])
+
+      setEditingId(_editingId)
+
+      return
     }
+
+    if (!_editingId) {
+      gridApiRef.current.updateRows([
+        {
+          id: itemId,
+          name: {
+            ...gridApiRef.current.getCellValue(itemId, 'name'),
+            isEditing: false,
+          },
+        },
+      ])
+
+      setEditingId(_editingId)
+
+      return
+    }
+
+    gridApiRef.current.updateRows([
+      {
+        id: editingId,
+        name: {
+          ...gridApiRef.current.getCellValue(editingId, 'name'),
+          isEditing: false,
+        },
+      },
+    ])
+
+    gridApiRef.current.updateRows([
+      {
+        id: _editingId,
+        name: {
+          ...gridApiRef.current.getCellValue(itemId, 'name'),
+          isEditing: true,
+        },
+      },
+    ])
+
+    setEditingId(_editingId)
+  }
+
+  const handleNameChange = (ev, itemId) => {
+    // for .isComposing see https://developer.mozilla.org/en-US/docs/Web/API/Element/keyup_event
+    // if (ev.isComposing || ev.code !== 'Enter') return
+    // dispatch(
+    //   updateName({
+    //     id: itemId,
+    //     name: ev.target.value,
+    //   })
+    // )
+
+    gridApiRef.current.updateRows([
+      {
+        id: editingId,
+        name: {
+          ...gridApiRef.current.getCellValue(itemId, 'name'),
+          isEditing: false,
+        },
+      },
+    ])
+
+    setEditingId(null)
+
+    // handleEdit(item.id)
   }
 
   return (
     <Container>
-      {items.map(item => (
+      <DataGrid
+        apiRef={gridApiRef}
+        columns={[
+          // {
+          //   field: 'status',
+          //   type: 'number',
+          // },
+          {
+            field: 'name',
+            renderCell: params => {
+              if (params.value.isEditing) {
+                return (
+                  <TextFieldStyled
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    defaultValue={params.value.name}
+                    onKeyUp={ev => {
+                      handleNameChange(ev, params.row.id)
+                    }}
+                  />
+                )
+              }
+
+              return <label>{params.value.name}</label>
+            },
+          },
+          {
+            field: 'edit',
+            renderCell: params => {
+              return (
+                <button
+                  onClick={() => {
+                    handleEdit(params.row.id)
+                  }}
+                >
+                  edit
+                </button>
+              )
+            },
+          },
+          // {
+          //   field: 'delete',
+          // },
+        ]}
+        rows={items.map(item => ({
+          id: item.id,
+          name: {
+            isEditing: item.id === editingId,
+            name: item.name,
+          },
+        }))}
+      ></DataGrid>
+      {/* {items.map(item => (
         <Item key={item.id} item={item} isEditing={editingId === item.id} handleEdit={handleEdit} />
-      ))}
+      ))} */}
     </Container>
   )
 }
@@ -36,4 +170,10 @@ const Container = styled.ul`
   padding: 0;
 
   list-style-type: none;
+`
+
+const TextFieldStyled = styled(TextField)`
+  & .MuiFilledInput-root .MuiFilledInput-input {
+    padding: 0;
+  }
 `
