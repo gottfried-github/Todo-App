595ba621e9cf1a32630bc0f354fcd8da59c64040
gottfried <gottfried.dev@gmail.com>
2024-02-21 18:25:21 +0200

    client, wip: slice - add update/delete reducers

--- client/src/store/slice.js
+++ client/src/store/slice.js
@@ -1,5 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+import { ITEM_STATUS } from '../constants'
+
 const slice = createSlice({
   name: 'todos',
   initialState: {
@@ -31,6 +33,64 @@ const slice = createSlice({
     },
     setError: (state, action) => {
       state.error = action.payload
+    },
+    append: (state, action) => {
+      if (state.filter.status === null || action.payload.status === state.filter.status) {
+        state.items.push(action.payload)
+      }
+
+      state.counters.all++
+
+      if (action.payload.status === ITEM_STATUS.DONE) {
+        state.counters.done++
+      } else {
+        state.counters.notDone++
+      }
+    },
+    updateItem: (state, action) => {
+      state.items = state.items.map(item => {
+        if (item.id === action.payload.id) {
+          return {
+            ...item,
+            ...action.payload.fields,
+          }
+        }
+
+        return item
+      })
+
+      if (state.filter.status) {
+        state.items = state.items.filter(item => item.status === state.filter.status)
+      }
+
+      if (action.payload.fields.status) {
+        if (action.payload.fields.status === ITEM_STATUS.DONE) {
+          state.counters.notDone--
+          state.counters.done++
+        } else {
+          state.counters.done--
+          state.counters.notDone++
+        }
+      }
+    },
+    deleteItem: (state, action) => {
+      const item = state.items[state.items.map(item => item.id).indexOf(action.payload)]
+
+      state.items = state.items.filter(item => item.id !== action.payload)
+
+      state.counters.all--
+
+      if (item.status === ITEM_STATUS.DONE) {
+        state.counters.done--
+      } else {
+        state.counters.notDone--
+      }
+    },
+    deleteDone: (state, action) => {
+      state.items = state.items.filter(item => item.status === 2)
+
+      state.counters.all -= action.payload.deletedCount
+      state.counters.done -= action.payload.deletedCount
     },
   },
   selectors: {
