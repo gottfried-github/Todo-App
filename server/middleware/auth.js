b0ef0c14bb4bf93ce5cef573e67e1d8d8a951dd9
gottfried <gottfried.dev@gmail.com>
2024-02-26 15:48:56 +0200

    wip: rename validateUser; add validateSignin

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,21 @@
-export async function validateUser(ctx, next) {
+export async function validateSignup(ctx, next) {
   if (!ctx.body.userName || !ctx.body.password) {
     ctx.throw(400, 'user name and password must be provided', {
       errors: {
         userName: !ctx.body.userName ? 'the field is required' : null,
+        password: !ctx.body.password ? 'the field is required' : null,
+      },
+    })
+  }
+
+  await next()
+}
+
+export async function validateSignin() {
+  if (!ctx.body.identifier || !ctx.body.password) {
+    ctx.throw(400, 'user name and password must be provided', {
+      errors: {
+        identifier: !ctx.body.identifier ? 'the field is required' : null,
         password: !ctx.body.password ? 'the field is required' : null,
       },
     })
