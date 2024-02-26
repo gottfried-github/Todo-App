866405b21cafc0d4acf14a195552b6a70436bf19
gottfried <gottfried.dev@gmail.com>
2024-02-26 16:46:26 +0200

    middleware, validateSignup: update

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,18 @@
 export async function validateSignup(ctx, next) {
-  if (!ctx.body.userName || !ctx.body.password) {
+  if (
+    !ctx.body.userName ||
+    !ctx.body.password ||
+    !ctx.body.firstName ||
+    !ctx.body.lastName ||
+    !ctx.body.email
+  ) {
     ctx.throw(400, 'user name and password must be provided', {
       errors: {
         userName: !ctx.body.userName ? 'the field is required' : null,
         password: !ctx.body.password ? 'the field is required' : null,
+        firstName: !ctx.body.firstName ? 'the field is required' : null,
+        lastName: !ctx.body.lastName ? 'the field is required' : null,
+        email: !ctx.body.email ? 'the field is required' : null,
       },
     })
   }
