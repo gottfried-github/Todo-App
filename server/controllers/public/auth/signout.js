23128e514e85cbc13fba185a8e2ff2206aef18ec
gottfried <gottfried.dev@gmail.com>
2024-02-26 15:23:40 +0200

    wip: sketch out signout controller

---
+++ server/controllers/public/auth/signout.js
@@ -0,0 +1,30 @@
+import User from '../../../models/user.js'
+
+export default async function signout(ctx) {
+  if (!ctx.cookies.get('jwt')) {
+    ctx.send(400, 'no refresh token')
+    return
+  }
+
+  const refreshToken = ctx.cookies.get('jwt')
+
+  // Is refreshToken in the database?
+  const user = await User.findOne({ refreshToken })
+
+  if (!user) {
+    ctx.cookies.set('jwt')
+    res.send(200, 'user is not signed in')
+    return
+  }
+
+  // Delete refreshToken in the database
+  user.refreshToken = ''
+
+  try {
+    await foundUser.save()
+    ctx.cookies.set('jwt')
+    ctx.send(200, 'signed out')
+  } catch (e) {
+    ctx.throw(500, 'database errored')
+  }
+}
