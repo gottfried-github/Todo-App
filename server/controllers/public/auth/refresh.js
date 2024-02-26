28b30ffd6b97b0829cd000e293e53577ee11d076
gottfried <gottfried.dev@gmail.com>
2024-02-26 15:10:55 +0200

    wip: sketch out refresh token controller

---
+++ server/controllers/public/auth/refresh.js
@@ -0,0 +1,26 @@
+import User from '../../../models/user.js'
+
+export default async function refresh(ctx) {
+  if (!ctx.cookies.get('jwt')) {
+    ctx.throw(401, 'no refresh token')
+  }
+
+  const refreshToken = ctx.cookies.get('jwt')
+
+  const user = await User.findOne({ refreshToken })
+  if (!user) {
+    ctx.throw(403, 'no user matches the refresh token')
+  }
+
+  jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, e => {
+    if (e) {
+      ctx.throw(403, 'refresh token is invalid')
+    }
+
+    const accessToken = jwt.sign({ userName: user.userName }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: process.env.JWT_ACCESS_EXPIRE / 1000,
+    })
+
+    ctx.send(200, { accessToken })
+  })
+}
