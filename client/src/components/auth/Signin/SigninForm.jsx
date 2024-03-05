ef1162d48ce529f81273d5aa1587aa1950f618f5
gottfried <gottfried.dev@gmail.com>
2024-03-05 13:59:04 +0200

    client, wip: start writing auth forms using React Final Form

--- client/src/components/auth/Signin/SigninForm.jsx
+++ client/src/components/auth/Signin/SigninForm.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Form, Field } from 'react-final-form'
 import TextField from '@mui/material/TextField'
 import { Button } from '../Signup/SignupForm'
-import Form from '../AuthForm'
+import AuthForm from '../AuthForm'
 
 import { signin as actionSignin } from '../../../store/actions/auth'
 import sliceAuth from '../../../store/store/slice-auth'
 
-export default function Signup() {
+export default function Signin() {
   const dispatch = useDispatch()
   const error = useSelector(state => sliceAuth.selectors.selectErrorSignin(state))
 
@@ -27,64 +28,117 @@ export default function Signup() {
     password: v => (v.length >= 8 ? null : 'field must be at least 8 characters long'),
   }
 
-  const submitCb = ev => {
-    ev.preventDefault()
-
-    if (validators.identifier(identifier) || validators.password(password)) return
-
-    dispatch(
-      actionSignin({
-        identifier,
-        password,
-      })
-    )
+  const submitCb = values => {
+    console.log('submitCb, values:', values)
+    // ev.preventDefault()
+    // if (validators.identifier(identifier) || validators.password(password)) return
+    // dispatch(
+    //   actionSignin({
+    //     identifier,
+    //     password,
+    //   })
+    // )
   }
 
   return (
-    <Form onSubmit={submitCb}>
-      <TextField
-        variant="outlined"
-        fullWidth
-        label="Username or Email"
-        placeholder={'ed@mail'}
-        defaultValue={identifier || ''}
-        error={
-          !!error?.errors?.identifier || (identifier ? !!validators.identifier(identifier) : false)
-        }
-        helperText={
-          error?.errors?.identifier?.message ||
-          (identifier ? validators.identifier(identifier) : null)
-        }
-        onInput={ev => {
-          if (error) {
-            dispatch(sliceAuth.actions.unsetErrorSignin())
-          }
+    <Form
+      onSubmit={submitCb}
+      initialValues={{
+        identifier: '',
+        password: '',
+      }}
+      validate={values => {
+        console.log('Form, validate, values:', values)
 
-          setIdentifier(ev.target.value)
-        }}
-      />
-      <TextField
-        variant="outlined"
-        fullWidth
-        label="Password"
-        type="password"
-        defaultValue={password || ''}
-        error={!!error?.errors?.password || (password ? !!validators.password(password) : false)}
-        helperText={
-          error?.errors?.password?.message || (password ? validators.password(password) : null)
+        const errors = {}
+
+        if (!values.identifier) {
+          errors.identifier = 'this field is required'
         }
-        onInput={ev => {
-          if (error) {
-            dispatch(sliceAuth.actions.unsetErrorSignin())
-          }
 
-          setPassword(ev.target.value)
-        }}
-      />
+        if (values.identifier?.length < 2) {
+          errors.identifier = 'user name must have at least two characters'
+        }
 
-      <Button type="submit" variant="contained">
-        sign in
-      </Button>
-    </Form>
+        if (!values.password) {
+          errors.password = 'this field is required'
+        }
+
+        if (values.password?.length < 8) {
+          errors.password = 'password must have at least 8 characters'
+        }
+
+        console.log('Form, validate, errors:', errors)
+
+        return errors
+      }}
+      render={({ handleSubmit }) => {
+        return (
+          <AuthForm onSubmit={handleSubmit}>
+            <Field
+              name="identifier"
+              render={({ input, meta }) => {
+                console.log('Field, identifier, meta.touched:', meta.touched)
+                return (
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    label="Username or Email"
+                    placeholder={'ed@mail'}
+                    name={input.name}
+                    value={input.value}
+                    error={!!error?.errors?.identifier || (meta.touched && !!meta.error)}
+                    helperText={
+                      error?.errors?.identifier?.message || (meta.touched && meta.error) || null
+                    }
+                    onChange={ev => {
+                      if (error) {
+                        dispatch(sliceAuth.actions.unsetErrorSignin())
+                      }
+
+                      input.onChange(ev)
+                    }}
+                    onBlur={input.onBlur}
+                    onFocus={input.onFocus}
+                  />
+                )
+              }}
+            />
+            <Field
+              name="password"
+              render={({ input, meta }) => {
+                return (
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    name={input.name}
+                    value={input.value}
+                    error={!!error?.errors?.password || (meta.touched && !!meta.error)}
+                    helperText={
+                      error?.errors?.password?.message || (meta.touched && meta.error) || null
+                    }
+                    onChange={ev => {
+                      if (error) {
+                        dispatch(sliceAuth.actions.unsetErrorSignin())
+                      }
+
+                      input.onChange(ev)
+                    }}
+                    onBlur={input.onBlur}
+                    onFocus={input.onFocus}
+                  />
+                )
+              }}
+            />
+
+            <Button type="submit" variant="contained">
+              sign in
+            </Button>
+          </AuthForm>
+        )
+      }}
+    ></Form>
   )
 }
