860ff0e32444947e530ebe76e0dab2a02124e0ec
gottfried <gottfried.dev@gmail.com>
2024-03-07 20:44:58 +0200

    client, wip: CreateTeam - add UI

--- client/src/components/team/CreateTeam.jsx
+++ client/src/components/team/CreateTeam.jsx
@@ -1,3 +1,79 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import styled from '@emotion/styled'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import Modal from '@mui/material/Modal'
+import { TextField } from '../todo/Form'
+
 export default function CreateTeam() {
-  return <div>Create Team</div>
+  const dispatch = useDispatch()
+  const [teamName, setTeamName] = useState('')
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const handleTeamNameChange = ev => {
+    setTeamName(ev.target.value)
+  }
+
+  const handleSubmit = ev => {
+    ev.preventDefault()
+    console.log('handleSubmit')
+  }
+
+  const handleOpen = () => {
+    setModalOpen(true)
+  }
+
+  const handleClose = () => {
+    setModalOpen(false)
+  }
+
+  return (
+    <>
+      <ButtonStyled variant="main" onClick={handleOpen}>
+        create a team
+      </ButtonStyled>
+      <ModalStyled open={modalOpen} onClose={handleClose}>
+        <PaperStyled elevation={8}>
+          <Form onSubmit={handleSubmit}>
+            <TextField
+              type="text"
+              variant="outlined"
+              fullWidth
+              placeholder="Team name"
+              value={teamName}
+              onChange={handleTeamNameChange}
+            />
+            <Button type="submit" variant="main">
+              Create Team
+            </Button>
+          </Form>
+        </PaperStyled>
+      </ModalStyled>
+    </>
+  )
 }
+
+const ButtonStyled = styled(Button)`
+  margin: auto;
+`
+
+const ModalStyled = styled(Modal)`
+  display: flex;
+  margin: 15px;
+`
+
+const PaperStyled = styled(Paper)`
+  width: 600px;
+  height: max-content;
+  padding: 15px;
+  margin: auto;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  row-gap: 16px;
+`
