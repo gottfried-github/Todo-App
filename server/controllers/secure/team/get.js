7ad312ce1c15e40e509f2c554908eaadbb3817e1
gottfried <gottfried.dev@gmail.com>
2024-03-07 17:03:51 +0200

    server, team controllers, get: finish

--- server/controllers/secure/team/get.js
+++ server/controllers/secure/team/get.js
@@ -2,8 +2,20 @@ import User from '../../../models/user.js'
 import Team from '../../../models/team.js'
 
 export default async function get(ctx) {
-  const team = Team.findById(ctx.params.teamId)
-  const users = User.find({ teamId: team._id })
+  try {
+    const team = Team.findById(ctx.params.teamId)
 
-  ctx.send(200, { ...team, members: users })
+    if (!team) {
+      ctx.send(404, "specified team doesn't exist")
+      return
+    }
+
+    const users = await User.find({ teamId: team._id })
+
+    const usersFiltered = users.filter(user => user._id !== ctx.state.user.id)
+
+    ctx.send(200, { ...team, members: usersFiltered })
+  } catch (e) {
+    ctx.throw(500, 'database errored', e)
+  }
 }
