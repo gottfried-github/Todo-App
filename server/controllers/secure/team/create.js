915af2e482cb3fec433e53e14da3cdf57affeb3e
gottfried <gottfried.dev@gmail.com>
2024-03-08 15:54:31 +0200

    server, team controllers, create: handle user already having a team

--- server/controllers/secure/team/create.js
+++ server/controllers/secure/team/create.js
@@ -5,6 +5,12 @@ import Team from '../../../models/team.js'
 
 export default async function create(ctx) {
   try {
+    const user = await User.findById(ctx.state.user.id)
+
+    if (user.teamId) {
+      ctx.throw(409, 'user already belongs to a team')
+    }
+
     const team = await Team.create({ ...ctx.request.body, author: ctx.state.user.id })
     const userUpdRes = await User.updateOne({ _id: ctx.state.user.id }, { teamId: team._id })
 
@@ -20,6 +26,10 @@ export default async function create(ctx) {
   } catch (e) {
     if (e instanceof mongoose.Error.ValidationError) {
       ctx.throw(400, 'validation error', e)
+    }
+
+    if (e.status) {
+      throw e
     }
 
     ctx.throw(500, 'database errored', e)
