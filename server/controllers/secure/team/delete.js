5b918e16cc01d4b16057ee43a7838f799af66525
gottfried <gottfried.dev@gmail.com>
2024-03-11 15:31:50 +0200

    server, team controllers, delete: dont use transactions

--- server/controllers/secure/team/delete.js
+++ server/controllers/secure/team/delete.js
@@ -1,15 +1,10 @@
-import mongoose from 'mongoose'
-
 import User from '../../../models/user.js'
 import Team from '../../../models/team.js'
 
 export default async function deleteTeam(ctx) {
   try {
-    await mongoose.connection.transaction(async session => {
-      await User.updateMany({ teamId: ctx.params.teamId }, { $unset: { teamId: 1 } }, { session })
-
-      await Team.deleteOne({ _id: ctx.params.teamId }, { session })
-    })
+    await User.updateMany({ teamId: ctx.params.teamId }, { $unset: { teamId: 1 } })
+    await Team.deleteOne({ _id: ctx.params.teamId })
 
     ctx.send(200, null, 'successfully deleted the team')
   } catch (e) {
@@ -17,6 +12,6 @@ export default async function deleteTeam(ctx) {
       throw e
     }
 
-    ctx.throw(500, 'database errored when trying to delete the team')
+    ctx.throw(500, 'database errored when trying to delete the team', e)
   }
 }
