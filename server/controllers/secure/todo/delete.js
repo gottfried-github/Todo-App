d8e62e50ded07075ca8c922f763ddfa7e475c578
gottfried <gottfried.dev@gmail.com>
2024-03-12 15:23:14 +0200

    server, todo controllers, delete: explicitly disallow deleting other users items

--- server/controllers/secure/todo/delete.js
+++ server/controllers/secure/todo/delete.js
@@ -4,10 +4,20 @@ import Todo from '../../../models/todo.js'
 
 export default async function deleteById(ctx) {
   try {
+    const item = await Todo.findById(ctx.params.id)
+
+    if (!item) {
+      ctx.throw(404, 'no item with given id')
+    }
+
+    if (item.userId !== ctx.state.user.id) {
+      ctx.throw(403, "the item doesn't belong to the current user")
+    }
+
     const _res = await Todo.deleteOne({ _id: ctx.params.id, userId: ctx.state.user.id })
 
     if (_res.deletedCount === 0) {
-      ctx.throw(404, 'no documents have been deleted')
+      ctx.throw(500, 'no documents have been deleted')
     }
 
     ctx.send(200, { deletedCount: _res.deletedCount })
