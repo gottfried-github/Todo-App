963d9ba4a3f765cdcd0059d079f8ddfe34471b67
gottfried <gottfried.dev@gmail.com>
2024-03-19 14:39:05 +0200

    cleanup

--- server/middleware/common.js
+++ server/middleware/common.js
@@ -1,7 +1,5 @@
 import jwt from 'jsonwebtoken'
 
-import User from '../models/user.js'
-
 export const authorize = async (ctx, next) => {
   if (!ctx.headers?.authorization?.startsWith('Bearer ')) {
     ctx.throw(401, "authorization header is absent or it's not Bearer")
