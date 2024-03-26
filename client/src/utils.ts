e12155d0ac3ed2e7768ba30eba8b6a47700722ee
gottfried <gottfried.dev@gmail.com>
2024-03-26 13:36:23 +0200

    wip, utils: add types

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,5 +1,7 @@
-export const validate = async (schema, values) => {
-  const errors = {}
+import { type ValidationError, type Schema } from 'yup'
+
+export const validate = async (schema: Schema, values: { [key: string]: unknown }) => {
+  const errors: { [key: string]: unknown } = {}
   let schemaErrors = null
 
   try {
@@ -10,8 +12,8 @@ export const validate = async (schema, values) => {
 
   if (!schemaErrors) return errors
 
-  for (const e of schemaErrors.inner) {
-    if (errors[e.path]) continue
+  for (const e of (schemaErrors as ValidationError).inner) {
+    if (e.path === undefined || errors[e.path]) continue
 
     errors[e.path] = e.errors[0]
   }
